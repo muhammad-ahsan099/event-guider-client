25a600e86a9cab9923e3b4d8fa02bd80755b9e15
Ahsan <ahsan@Ahsans-MacBook-Pro.local>
2023-04-10 22:18:22 +0500

    intial push

---
+++ src/screens/favorites/Favorties.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Favorties() {
+  return (
+    <View>
+      <Text>Favorties</Text>
+    </View>
+  )
+}
